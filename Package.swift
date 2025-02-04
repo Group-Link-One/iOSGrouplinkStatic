c932068d9527ae62f611d9964c68d96216526b67
Emmanuel Miranda <e.miranda@grouplinkone.com>
2025-02-03 21:49:29 -0300

    v5.3.11

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "GroupLinkStatic",
-                      url: "https://s3.us-west-004.backblazeb2.com/gl-ios-releases/StaticReleases/5.3.10.zip",
-                      checksum: "1fd957d8ef082083cc2dcb341254c044dd6fde5e6881baed9f5183d01058da41")
+                      url: "https://s3.us-west-004.backblazeb2.com/gl-ios-releases/StaticReleases/5.3.11.zip",
+                      checksum: "17d19147469cfeca3b2e931d8d5322a143c8deaec0b078069da4654af5cf2b25")
     ]
 )
